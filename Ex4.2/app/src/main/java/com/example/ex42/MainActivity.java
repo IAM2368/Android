ee33484fb28cf2c0cd426afbeafc9a263ab61515
Quỳnh Anh <nguyenquynhanh2682k3@gmail.com>
2025-05-16 00:59:19 +0700

    upload

--- Ex4.2/app/src/main/java/com/example/ex42/MainActivity.java
+++ Ex4.2/app/src/main/java/com/example/ex42/MainActivity.java
@@ -1,5 +1,8 @@
-package com.example.ex42;package com.example.bmi;
+package com.example.ex42;
+
 import java.text.DecimalFormat;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
@@ -10,6 +13,7 @@ public class MainActivity extends Activity {
     Button btnChandoan;
     EditText editTen, editChieucao, editCannang, editBMI, editChandoan;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,30 +29,38 @@ public class MainActivity extends Activity {
         btnChandoan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-                double H = Double.parseDouble(editChieucao.getText() + "");
-                double W = Double.parseDouble(editCannang.getText() + "");
-                double BMI = W / Math.pow(H, 2);
-                String chandoan = "";
-                if (BMI < 18)
-                    chandoan = "Bạn gầy";
-            }
-            else if (BMI <= 24.9) {
-                chandoan = "Bạn bình thường";
-            }
-            else if (BMI <= 29.9) {
-                chandoan = "Bạn béo phì độ 1";
-            }
-            else if (BMI <= 34.9) {
-                chandoan = "Bạn béo phì độ 2";
-            }
-            else {
-                chandoan = "Bạn béo phì độ 3";
-            }
+                try {
+                    double H = Double.parseDouble(editChieucao.getText().toString());
+                    double W = Double.parseDouble(editCannang.getText().toString());
 
-            DecimalFormat dcf = new DecimalFormat("#.0");
-            editBMI.setText(dcf.format(BMI));
-            editChandoan.setText(chandoan);
-        }
-    });
+                    // Nếu chiều cao nhập theo cm thì chuyển sang mét
+                    if (H > 3) {
+                        H = H / 100;
+                    }
+
+                    double BMI = W / Math.pow(H, 2);
+                    String chandoan = "";
+
+                    if (BMI < 18) {
+                        chandoan = "Bạn gầy";
+                    } else if (BMI <= 24.9) {
+                        chandoan = "Bạn bình thường";
+                    } else if (BMI <= 29.9) {
+                        chandoan = "Bạn béo phì độ 1";
+                    } else if (BMI <= 34.9) {
+                        chandoan = "Bạn béo phì độ 2";
+                    } else {
+                        chandoan = "Bạn béo phì độ 3";
+                    }
+
+                    DecimalFormat dcf = new DecimalFormat("#.0");
+                    editBMI.setText(dcf.format(BMI));
+                    editChandoan.setText(chandoan);
+                } catch (NumberFormatException e) {
+                    editBMI.setText("Lỗi");
+                    editChandoan.setText("Vui lòng nhập đầy đủ thông tin");
+                }
+            }
+        });
+    }
 }
