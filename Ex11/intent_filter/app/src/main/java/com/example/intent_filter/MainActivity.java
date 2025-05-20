56e53eda469e7a9b9b1e0a4f167d4aef6a280481
Quỳnh Anh <nguyenquynhanh2682k3@gmail.com>
2025-05-20 18:13:17 +0700

    upload

--- Ex11/intent_filter/app/src/main/java/com/example/intent_filter/MainActivity.java
+++ Ex11/intent_filter/app/src/main/java/com/example/intent_filter/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.intent_filter;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Intent intent = getIntent();
+        if (intent != null && intent.getData() != null) {
+            String url = intent.getData().toString();
+            // Open the URL in the browser
+            webView = findViewById(R.id.web_search);
+            webView.loadUrl(url);
+
+        }
+
     }
 }
